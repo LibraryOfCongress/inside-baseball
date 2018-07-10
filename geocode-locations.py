2ffb953629debdcfebc086f7bf74ba488f21cd6b
Chris Adams <cadams@loc.gov>
2018-07-10 17:25:26 -0400

    Geocoding cleanup
    * Use a more explanatory table name
    * Retrieve more records
    * Basic fuzzy matching for Subject records

--- geocode-locations.py
+++ geocode-locations.py
@@ -8,6 +8,7 @@ import sys
 
 import requests
 from csvs_to_sqlite.utils import table_exists
+from Levenshtein import ratio
 
 from utils import column_exists, normalize_place_name
 
@@ -15,41 +16,38 @@ GEONAMES_API_USERNAME = os.environ.get("GEONAMES_API_USERNAME", "acdha")
 
 db = sqlite3.connect(sys.argv[1], isolation_level=None)
 
-if not table_exists(db, "geonames"):
+if not table_exists(db, "geonames_queries"):
     db.execute(
         """
-        CREATE TABLE geonames (
+        CREATE TABLE geonames_queries (
             id INTEGER PRIMARY KEY,
             value VARCHAR(255),
             fetched TIMESTAMP NULL,
             result_count INTEGER,
-            geonames JSON NULL,
-            latitude VARCHAR(16) NULL,
-            longitude VARCHAR(16) NULL
+            geonames JSON NULL
         )
         """
     )
 
 for table_name in ("Location", "Geography", "Subject"):
-    if not column_exists(db, table_name, "geoname"):
-        db.execute(
-            f"ALTER TABLE {table_name} ADD COLUMN geoname INTEGER NULL REFERENCES geonames (id)"
-        )
+    if not column_exists(db, table_name, "latitude"):
+        db.execute(f"ALTER TABLE {table_name} ADD COLUMN latitude VARCHAR(16) NULL")
+        db.execute(f"ALTER TABLE {table_name} ADD COLUMN longitude VARCHAR(16) NULL")
 
 
 # Look for new terms to search for:
 
 source_queries = [
     """
-    SELECT value FROM Geography WHERE value NOT IN (SELECT value FROM geonames)
+    SELECT value FROM Geography WHERE value NOT IN (SELECT value FROM geonames_queries)
     """,
     """
-    SELECT value FROM Location WHERE value NOT IN (SELECT value FROM geonames)
+    SELECT value FROM Location WHERE value NOT IN (SELECT value FROM geonames_queries)
     """,
     """
     SELECT value FROM Subject
         WHERE
-            value NOT IN (SELECT value FROM geonames)
+            value NOT IN (SELECT value FROM geonames_queries)
             AND NOT value LIKE '%Team%'
     """,
 ]
@@ -60,18 +58,18 @@ for sql in source_queries:
 
         db.execute(
             """
-            INSERT INTO geonames (value)
-                SELECT ? WHERE NOT EXISTS (SELECT value FROM geonames WHERE value = ?)
+            INSERT INTO geonames_queries (value)
+                SELECT ? WHERE NOT EXISTS (SELECT value FROM geonames_queries WHERE value = ?)
             """,
             (query_value, query_value),
         )
 
 for (value,) in db.execute(
-    "SELECT value FROM geonames WHERE fetched IS NULL"
+    "SELECT value FROM geonames_queries WHERE fetched IS NULL"
 ).fetchall():
     filters = {
         "username": GEONAMES_API_USERNAME,
-        "maxRows": "3",
+        "maxRows": 10,
         "q": value,
         "country": "US",
     }
@@ -88,36 +86,72 @@ for (value,) in db.execute(
 
     result_count = int(data["totalResultsCount"])
 
-    if result_count:
-        first_result = data["geonames"][0]
-        lat = first_result["lat"]
-        lng = first_result["lng"]
-        geonameId = first_result["geonameId"]
-    else:
-        lat = lng = geonameId = ""
-
     db.execute(
         """
-            UPDATE geonames
+            UPDATE geonames_queries
                 SET fetched = CURRENT_TIMESTAMP,
                     result_count = ?,
-                    geonames = ?,
-                    latitude = ?,
-                    longitude = ?
+                    geonames = ?
                 WHERE value = ?;
         """,
-        (result_count, json.dumps(data["geonames"]), lat, lng, value),
+        (result_count, json.dumps(data["geonames"]), value),
     )
 
-    print(value, result_count, geonameId, lat, lng)
+    print(value, result_count, [i["name"] for i in data["geonames"]])
 
-for table_name in ("Geography", "Location", "Subject"):
-    res = db.execute("SELECT value FROM %s WHERE geoname IS NULL" % table_name)
+
+for table_name in ("Geography", "Location"):
+    res = db.execute("SELECT value FROM %s" % table_name)
     for (value,) in res.fetchall():
         query_value = normalize_place_name(value)
+
+        geonames_query = db.execute(
+            "SELECT geonames FROM geonames_queries WHERE result_count > 0 AND value = ?",
+            (query_value,),
+        ).fetchone()
+
+        if geonames_query:
+            geonames = json.loads(geonames_query[0])
+            lat = geonames[0]["lat"]
+            lng = geonames[0]["lng"]
+
+            db.execute(
+                f"UPDATE {table_name} SET latitude = ?, longitude = ? WHERE value = ?",
+                (lat, lng, value),
+            )
+
+# Subjects are messy and we might want to be careful about matches:
+for (value,) in db.execute("SELECT value FROM Subject").fetchall():
+    query_value = normalize_place_name(value)
+
+    geoname_lookup = db.execute(
+        "SELECT id, geonames FROM geonames_queries WHERE value = ?", (query_value,)
+    ).fetchone()
+
+    if not geoname_lookup:
+        continue
+
+    geoname_id, geonames = geoname_lookup
+
+    geonames = json.loads(geonames)
+
+    for geoname in geonames:
+        name = geoname["name"] or geoname["toponymName"]
+        match_ratio = ratio(value, name)
+
+        if match_ratio > 0.8:
+            lat = geoname["lat"]
+            lng = geoname["lng"]
+            db.execute(
+                "UPDATE Subject SET latitude = ?, longitude = ? WHERE value = ?",
+                (lat, lng, value),
+            )
+            break
+    else:
         db.execute(
-            f"UPDATE {table_name} SET geoname = (SELECT id FROM geonames WHERE value = ?) WHERE value = ?",
-            (query_value, value),
+            "UPDATE Subject SET latitude = NULL, longitude = NULL WHERE value = ?",
+            (value,),
         )
+
 
 db.close()
