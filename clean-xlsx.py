63decb769d7ae9919d6d5501f2ca798cc7598d3f
Chris Adams <chris@improbable.org>
2018-07-10 10:17:26 -0400

    Initial conversion script

---
+++ clean-xlsx.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from openpyxl import load_workbook
+
+for xlsx in sys.argv[1:]:
+    wb = load_workbook(filename=xlsx)
+
+    for worksheet in wb.worksheets:
+        for row in worksheet.rows:
+            for cell in row:
+                if cell.data_type in ('s', ):
+                    cell.value = cell.value.strip()
+
+    wb.save(filename='%s-cleaned%s' % os.path.splitext(xlsx))
