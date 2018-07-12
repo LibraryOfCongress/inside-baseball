9df22b5cfd53578eae8f73fec008549ea1e68ad9
Chris Adams <cadams@loc.gov>
2018-07-12 11:54:22 -0400

    Update viewer to display items in the new format

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1,3 +1,19 @@
+let $ = (selector, scope = document) => {
+    return scope.querySelector(selector);
+};
+
+let $$ = (selector, scope = document) => {
+    return Array.from(scope.querySelectorAll(selector));
+};
+
+function removeChildren(element) {
+    while (element.firstChild) {
+        element.firstChild.remove();
+    }
+}
+
+let allPoints, allItems;
+
 let baseballIcon = L.icon({
     iconUrl: "static/icons/baseball-ball-solid.svg",
     iconSize: [32, 32],
@@ -14,34 +30,29 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch("InsideBaseball.geojson")
+let itemLayer = L.layerGroup().addTo(map);
+
+fetch("InsideBaseball.json")
     .then(response => {
         return response.json();
     })
     .then(data => {
-        L.geoJSON(data, {
-            onEachFeature: (feature, layer) => {
-                let popup = `
-                <h1>${feature.properties.title}</h1>
+        allPoints = data.points;
+        allItems = data.items;
 
-                <div>${feature.properties.metadata.Description}</div>
+        allPoints.forEach(point => {
+            let title = point.title || JSON.stringify(point.latlng);
 
-                `.trim();
-
-                if (feature.properties.metadata.Copyright) {
-                    popup += `<div><b>Copyright:</b> ${
-                        feature.properties.metadata.Copyright
-                    }</div>`;
-                }
-
-                layer.bindPopup(popup.trim());
-            },
-            pointToLayer: (feature, latlng) => {
-                return L.marker(latlng, {
-                    title: feature.properties.title
-                });
-            }
-        }).addTo(map);
+            L.marker(point.latlng, {
+                title: title,
+                items: point.items
+            })
+                .bindPopup(title)
+                .on("click", function(evt) {
+                    displayItems(evt.target.options.items);
+                })
+                .addTo(itemLayer);
+        });
     });
 
 fetch(
@@ -63,10 +74,32 @@ fetch(
             }
         });
 
-        ballparksLayer.addTo(map);
-
         // Now that we have a layer, we'll add a control for toggling them:
         L.control.layers(null, { "Present Day Ballparks": ballparksLayer }).addTo(map);
     });
 
 map.setView({ lat: 38.8879105, lng: -77.0024652 }, 7);
+
+let itemListing = $("#viewer-items > ul");
+
+function displayItems(itemIDs) {
+    removeChildren(itemListing);
+
+    itemIDs.forEach(itemID => {
+        let item = allItems[itemID];
+        // Convenience aliases
+        let title = item.title;
+        let meta = item.metadata;
+
+        let itemContainer = document.createElement("li");
+        itemContainer.classList.add("list-group-item");
+        itemListing.appendChild(itemContainer);
+
+        let itemLink = document.createElement("a");
+        itemLink.textContent = title;
+        itemLink.href = meta["Digital ID URL"];
+        itemLink.target = "_blank";
+
+        itemContainer.appendChild(itemLink);
+    });
+}
