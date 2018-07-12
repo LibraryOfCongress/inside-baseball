0c220044ab6ad759fae087c32132237d3e0f8159
Chris Adams <cadams@loc.gov>
2018-07-12 11:52:51 -0400

    Change the data export format to group on places
    This breaks GeoJSON compatibility but allows us to have
    fewer pins on the map and items listed in multiple places
    without duplication

--- export-json.py
+++ export-json.py
@@ -11,19 +11,37 @@ def export_items_as_geojson(db_file, output_file):
     db = sqlite3.connect("file:%s?mode=ro" % db_file, uri=True)
     db.row_factory = sqlite3.Row
 
-    items = []
+    all_points = {}
+    all_items = {}
 
-    for item in db.execute("SELECT * FROM Items LIMIT 100").fetchall():
-        items.append(export_item_as_geojson(db, item))
+    for item in db.execute(
+        "SELECT rowid, * FROM Items WHERE Items.'Object Type' != 'Web Page'"
+    ).fetchall():
+        points, item = export_item(db, item)
+
+        item_id = item["id"]
+        all_items[item_id] = item
+
+        for point in points:
+            combined_point = all_points.setdefault(
+                point["latlng"], {"latlng": point["latlng"]}
+            )
+            combined_point.setdefault("items", set()).add(item_id)
+            if not combined_point.get("title"):
+                combined_point["title"] = point["title"]
+
+    for point in all_points.values():
+        # Sets are not serializable by the default JSON encoder
+        point["items"] = list(point["items"])
 
     with open(output_file, "w") as output_f:
-        json.dump({"type": "FeatureCollection", "features": items}, output_f)
+        json.dump({"points": list(all_points.values()), "items": all_items}, output_f)
 
 
 MULTI_VALUE_COLUMN_RE = re.compile(r"^(.+)_\d+$")
 
 
-def export_item_as_geojson(db, item):
+def export_item(db, item):
     metadata = {}
 
     for k in item.keys():
@@ -52,11 +70,11 @@ def export_item_as_geojson(db, item):
 
     display_titles = list(filter(None, titles))
     if not display_titles:
-        title = metadata["Object Number"]
+        title = "Row #%d" % metadata["rowid"]
     else:
         title = display_titles[0]
 
-    all_coords = []
+    all_coords = dict()
 
     for lookup_name in (
         "Creator/Publisher",
@@ -79,7 +97,7 @@ def export_item_as_geojson(db, item):
         else:
             lookup_table = lookup_name
 
-        result = db.execute(f"SELECT * FROM {lookup_table} WHERE ROWID IN ({db_ids})")
+        result = db.execute(f"SELECT * FROM {lookup_table} WHERE rowid IN ({db_ids})")
         resolved_values = []
 
         for row in result.fetchall():
@@ -92,23 +110,17 @@ def export_item_as_geojson(db, item):
                 continue
 
             if lat and lng:
-                all_coords.append((float(lng), float(lat)))
+                all_coords[(lat, lng)] = row["value"]
 
         metadata[lookup_name] = resolved_values
 
-    if all_coords:
-        coords = all_coords[0]
-    else:
-        coords = [0, 0]
-
-    return {
-        "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": coords},
-        "properties": {"title": title, "metadata": metadata},
-    }
+    return (
+        [{"title": v, "latlng": k} for k, v in all_coords.items()],
+        {"title": title, "metadata": metadata, "id": metadata["rowid"]},
+    )
 
 
 if __name__ == "__main__":
     for f in sys.argv[1:]:
-        output_filename = "%s.geojson" % os.path.splitext(f)[0]
+        output_filename = "%s.json" % os.path.splitext(f)[0]
         export_items_as_geojson(f, output_filename)
